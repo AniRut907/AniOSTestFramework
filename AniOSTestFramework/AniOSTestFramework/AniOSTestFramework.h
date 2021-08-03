ab9a6cf418c0ec4396175a5aeff49206614ff616
Aniket Lokhande <Aniket.Lokhande@extentia.com>
2021-08-03 17:16:59 +0530

    initial commit

---
+++ AniOSTestFramework/AniOSTestFramework/AniOSTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  AniOSTestFramework.h
+//  AniOSTestFramework
+//
+//  Created by Aniket Lokhande on 03/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AniOSTestFramework.
+FOUNDATION_EXPORT double AniOSTestFrameworkVersionNumber;
+
+//! Project version string for AniOSTestFramework.
+FOUNDATION_EXPORT const unsigned char AniOSTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AniOSTestFramework/PublicHeader.h>
+
+
